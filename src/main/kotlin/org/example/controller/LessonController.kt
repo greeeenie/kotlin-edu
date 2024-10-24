e81b236df0b4935234bf441b2534ae432c4818d0
greeeenie <stepankolotilin@gmail.com>
2024-10-24 23:40:56 +0600

    Add new controllers

--- src/main/kotlin/org/example/controller/LessonController.kt
+++ src/main/kotlin/org/example/controller/LessonController.kt
@@ -2,6 +2,8 @@ package org.example.controller
 
 import org.example.dto.LessonDto
 import org.example.service.LessonService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -9,30 +11,34 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 
 @RestController
 @RequestMapping("/lessons")
 class LessonController(
     private val lessonService: LessonService
 ) {
-    @GetMapping("/all")
-    fun getAll(): List<LessonDto> {
-        return lessonService.getAll()
-    }
+    @GetMapping
+    fun getAll(): ResponseEntity<List<LessonDto>> = ResponseEntity.ok(lessonService.getAll())
 
-    @PostMapping("/create")
-    fun create(@RequestBody lessonDto: LessonDto): Int? {
-        return lessonService.create(lessonDto)
+    @PostMapping
+    fun create(@RequestBody lessonDto: LessonDto): ResponseEntity<LessonDto> {
+        val createdLesson = lessonService.create(lessonDto)
+        return ResponseEntity.created(
+            ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(createdLesson.id)
+                .toUri()
+        ).body(createdLesson)
     }
 
     @PutMapping("/{id}")
-    fun update(@PathVariable id: Int, @RequestBody lessonDto: LessonDto) {
-        return lessonService.update(id, lessonDto)
-    }
+    fun update(@PathVariable id: Int, @RequestBody lessonDto: LessonDto): ResponseEntity<LessonDto> = ResponseEntity.ok(lessonService.update(id, lessonDto))
 
     @DeleteMapping("/{id}")
-    fun delete(@PathVariable id: Int) {
-        return lessonService.delete(id)
-    }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete(@PathVariable id: Int) = lessonService.delete(id)
+
 }
